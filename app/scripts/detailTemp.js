f18d70e09f4522d8f7a55ab5612463e4271649a4
Jones-Mtz <daesmart@outlook.com>
2015-09-06 00:17:16 -0500

    temperatura y humedad

--- app/scripts/detailTemp.js
+++ app/scripts/detailTemp.js
@@ -28,9 +28,33 @@ var data = {
 var options = {
     responsive:true
 };
+
+var randomnb = function(){ return Math.round(Math.random()*300)};
+var datas = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "Dados primários",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [randomnb(), randomnb(), randomnb(), randomnb(), randomnb(), randomnb(), randomnb()]
+        },
+        {
+            label: "Dados secundários",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, randomnb(), randomnb(), randomnb()]
+        }
+    ]
+}; 
+
 // Get context with jQuery - using jQuery's .get() method.
 var ctx = $("#chartTemperatura").get(0).getContext("2d");
 var ctx2 = $("#chartActuadores").get(0).getContext("2d");
 // This will get the first returned node in the jQuery collection.
 var myLineChart = new Chart(ctx).Line(data, options);
-var myBarChart = new Chart(ctx2).Bar(data, options);
+var myBarChart = new Chart(ctx2).Bar(datas, options);
